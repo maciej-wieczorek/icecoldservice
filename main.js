69e550209e15769bb1e2969a803726eba99a706a
Szymon3125 <50416224+Szymon3125@users.noreply.github.com>
2019-05-16 10:08:24 +0200

    js

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ function space(){
     h1 = parseFloat(h1);
     h2 = parseFloat(h2);
     var h3 = h1 + h2;
-    console.log(h1+", "+h2+", "+h3);
     var len = document.getElementsByClassName("margintop").length;
     for(i = 0; i < len; i++)
         document.getElementsByClassName("margintop")[i].style.marginTop = h3+"px";
