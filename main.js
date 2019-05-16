f63f058588214cca06ab7ff7f768fdb905b498f9
Szymon3125 <50416224+Szymon3125@users.noreply.github.com>
2019-05-16 09:50:06 +0200

    js

---
+++ main.js
@@ -0,0 +1,14 @@
+function space(){
+    var img = document.querySelector('.carousel');
+    var nav = document.querySelector('.navbar');
+    var h1 = getComputedStyle(img).height;
+    var h2 = getComputedStyle(nav).height;
+    h1 = parseFloat(h1);
+    h2 = parseFloat(h2);
+    var h3 = h1 + h2;
+    console.log(h1+", "+h2+", "+h3);
+    var len = document.getElementsByClassName("margintop").length;
+    document.getElementsByClassName("margintop")[0].style.marginTop = h3+"px";
+    for(i = 1; i < len; i++)
+        document.getElementsByClassName("margintop")[i].style.marginTop = h1+"px";
+}
