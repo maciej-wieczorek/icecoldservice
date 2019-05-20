60409b0c616f3486570bf02607801facc44884e7
Maciej <38870449+maciej-wieczorek@users.noreply.github.com>
2019-05-20 10:56:42 +0200

    smaller margin

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-function space(){
-    var img = document.querySelector('.carousel');
-    var nav = document.querySelector('.navbar');
-    var h1 = getComputedStyle(img).height;
-    var h2 = getComputedStyle(nav).height;
-    h1 = parseFloat(h1);
-    h2 = parseFloat(h2);
-    var h3 = h1 + h2;
-    var len = document.getElementsByClassName("margintop").length;
-    for(i = 0; i < len; i++)
-        document.getElementsByClassName("margintop")[i].style.marginTop = h3+"px";
+function space(){
+
+    var img = document.querySelector('.carousel');
+
+    var nav = document.querySelector('.navbar');
+
+    var h1 = getComputedStyle(img).height;
+
+    var h2 = getComputedStyle(nav).height;
+
+    h1 = parseFloat(h1);
+
+    h2 = parseFloat(h2);
+
+    var h3 = h1 + h2;
+
+    var len = document.getElementsByClassName("margintop").length;
+
+    document.getElementsByClassName("margintop")[0].style.marginTop = h3+"px";
+
+    for(i = 1; i < len; i++)
+
+        document.getElementsByClassName("margintop")[i].style.marginTop = (h1 / 2) + "px";
+
 }
